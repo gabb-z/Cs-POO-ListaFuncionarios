9d861f43d4a61c4f272d6bb0cbbb8458de06b5f7
gabb-z <gabbz.liima@gmail.com>
2023-03-27 21:07:01 -0300

    v1 - Projeto ListaFuncionarios

---
+++ ListaFuncionarios/Funcionario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ListaFuncionarios
+{
+    class Funcionario
+    {
+        //Declaração de Atributos da Classe - Dados do Funcionário
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salario { get; private set; }
+
+        //Construtor da Classe - Dados do Funcionário
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        //Função - Aumentar Salário
+        public void AumentarSalario(double porcentagem)
+        {
+            Salario += (porcentagem * Salario) / 100.0; 
+        }
+
+        //ToString - Dados do Funcionário
+        public override string ToString()
+        {
+            return Id + ", " + Nome + ", R$ " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ListaFuncionarios/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace ListaFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de Dados - Quantidade de Funcionários
+            Console.Write("Quantos funcionários deseja cadastrar: ");
+            int N = int.Parse(Console.ReadLine());
+
+            //Declaração de Lista - Dados do Funcionário
+            List<Funcionario> listaFuncionario = new List<Funcionario>();
+
+            //Condição - Entrada de Dados do Funcionário
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine($"\nEmpregado #{i}:");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                listaFuncionario.Add(new Funcionario(id, nome, salario));
+            }
+
+            //Entrada de Dados - Aumentar Salário
+            Console.Write("\nDigite o ID do funcionário que deseja aumentar o salário: ");
+            int buscarId = int.Parse(Console.ReadLine());
+            Funcionario func = listaFuncionario.Find(x => x.Id == buscarId);
+
+            //Condição - Buscar ID
+            if (func != null)
+            {
+                Console.Write("\nDigite a porcentagem que deseja alterar o salário do funcionário: ");
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.AumentarSalario(porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("\nID inválido!");
+            }
+
+            //Saída de Dados do Funcionário
+            Console.WriteLine("\nLista de funcionário(s) atualizada: \n");
+            foreach (Funcionario obj in listaFuncionario)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
